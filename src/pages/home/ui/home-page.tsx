b6454490f98a38366796709fb4c7bf1379bef43b
Orh <ohthatorh@yandex.ru>
2025-08-03 19:31:40 +0300

    initial

---
+++ src/pages/home/ui/home-page.tsx
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material";
+
+export default function HomePage() {
+  return (
+    <main>
+      <Typography component="p">asdasasdadsads</Typography>
+    </main>
+  );
+}
